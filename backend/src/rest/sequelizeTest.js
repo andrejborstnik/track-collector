b93a5747b1a2e13486aea53b98669d1d25216c83
Žiga Zupančič <zigazupancic5@gmail.com>
2017-04-08 20:41:27 +0200

    Add sequelizeTest.js.

---
+++ backend/src/rest/sequelizeTest.js
@@ -0,0 +1,36 @@
+//
+// Sequelize test
+//
+
+'use strict';
+
+var Sequelize = require('sequelize');
+
+var config = require('config');
+
+var sequelize = new Sequelize('gps', 'gps', 'gps-tracks', {
+    host: 'localhost',
+    dialect: 'postgresql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }});
+
+
+
+var User = sequelize.define('user', {
+    username: Sequelize.STRING,
+    birthday: Sequelize.DATE
+});
+
+sequelize.sync({force: true}).then(function() {
+    return User.create({
+        username: 'janedoe',
+        birthday: new Date(1980, 6, 20)
+    });
+}).then(function(jane) {
+    console.log(jane.get({
+        plain: true
+    }));
+});
