6f8ae4147ff47c56ec5837dd35c45d7c8f7ec5b6
andrejborstnik <andrej-borstnik@hotmail.com>
2017-04-21 17:31:51 +0200

    Alen tracker.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,7 @@ const express = require('express');
 const async = require('co-express');
 const router = express.Router();
 
+const request = require('request-promise-native');
 // Setup Cross Origin Resource Sharing.
 const cors = require('cors');
 router.use(cors());
@@ -38,6 +39,31 @@ router.get('/api/queryDb/:query', function (req, res) {
     }).catch(function (err) {
         w.error(err);
         res.status(400).send(err);
+    });
+});
+
+router.get('/api/track/:query', function (req, res) {
+    let query = decodeURI(req.params.query);
+    let params = decodeURI(req.query.params); // todo parse safely
+
+    request({
+        method: "POST",
+        json: true,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        uri:'https://test.goopti.com/tracker/test/gpsQuery',
+        body: {
+          deviceId: "string",
+          startDate: "2017-04-21T09:15:00Z",
+          endDate: "2017-04-21T12:00:00.00Z",
+          userId: "string"
+        }
+    }).then((body) => {
+        res.send(body);
+    }).catch((err) => {
+        throw err;
     });
 });
 
