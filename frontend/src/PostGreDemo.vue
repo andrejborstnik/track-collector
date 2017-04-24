db637249b2e34868d51fa193a16f8c6a379751b8
Miha Eleršič <miha.elersic@gmail.com>
2017-04-24 20:11:59 +0000

    gumbpf

--- frontend/src/PostGreDemo.vue
+++ frontend/src/PostGreDemo.vue
@@ -1,10 +1,9 @@
 <template>
 
     <div>
-        <span>Insert a query: <input v-model="query" @keyup.prevent.enter="queryDatabase" style="border: 1px solid gray;" placeholder="select * from users"></span><br>
-
-
-
+        <span>Insert a query: <input v-model="query" @keyup.prevent.enter="queryDatabase" style="border: 1px solid gray;"></span>
+        <button v-on:click="queryDatabase">GO!</button>
+        <br>
         <template v-if="output">
             <span>Result (raw): {{output}}</span>
             <br>
@@ -14,7 +13,8 @@
                 <tbody>
                     <tr>
                         <th>#</th>
-                        <th v-for="(value,key,i) in output[0]">     {{key}}
+                        <th v-for="(value,key,i) in output[0]">
+                            {{key}}
                         </th> 
                     </tr>
                     <tr v-for="(x,idx) in output">
@@ -23,13 +23,9 @@
                             {{value}}
                         </td>
                     </tr>
-
                 </tbody>
             </table> 
         </template>
-
-        
-
 
     </div>
 
@@ -37,7 +33,17 @@
 
 
 <style lang="scss">
-    
+button {
+    background-color: #e0e0e0;
+    border: none;
+    color: black;
+    padding: 5px 5px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+
 </style>
 
 
@@ -82,11 +88,7 @@
         data () {
             return {
                 query: 'select * from users',
-                output: '',
-                    items: [
-                           { message: 'Foo' },
-                           { message: 'Bar' }
-                           ]
+                output: ''
             }
         }
     }
