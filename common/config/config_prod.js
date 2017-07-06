1e50dfa7f5343425dbbfc41b746f0aed5f6b52e3
andrejborstnik <andrej-borstnik@hotmail.com>
2017-07-06 15:50:54 +0200

    ..

--- common/config/config_prod.js
+++ common/config/config_prod.js
@@ -9,10 +9,3 @@
 // CONFIG
 //
 
-exports.url_prefix = `${exports.path_prefix == '/' ? '' : exports.path_prefix}`;
-
-// Base URI for ALL backend services.
-exports.base_uri = `${exports.path_prefix == '/' ? '' : exports.path_prefix}/api`;
-
-// Frontend API prefix.
-exports.paths_api_prefix = `${exports.fe_url}${exports.path_prefix == '/' ? '' : exports.path_prefix}/api`;
